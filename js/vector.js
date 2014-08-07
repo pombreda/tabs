ae86c9850ca5da3d25f6cc831f2e9867a9dd0b32
Kelsey Jordahl <kjordahl@enthought.com>
2014-08-07 14:14:19 -0700

    Simple static 'vector' lines

--- js/vector.js
+++ js/vector.js
@@ -19,3 +19,25 @@ var featureLayer = L.mapbox.featureLayer()
 	})
     })
     .addTo(map);
+
+function vectorLayer(points) {
+    var vectors = [];
+    var dx = 0.1;
+    var dy = 0.1;
+    for (var i=0; i<points.length; i++) {
+	endpoint = [points[i][0] + dx, points[i][1] + dy];
+	vectors.push([points[i], endpoint]);
+    }
+    console.log(vectors);
+    L.multiPolyline(vectors).addTo(map);
+}
+
+$.getJSON('json_data/grd_locations.json', function(json) {
+    var points = [];
+    for (var i=0; i<json['lat'].length; i++) {
+	points.push([json.lat[i], json.lon[i]]);
+    }
+    vectorLayer(points);
+});
+
+//var points = gridPoints();
