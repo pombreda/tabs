a1cc10bd6590ee657c058eec91eb8345d888f07b
Kelsey Jordahl <kjordahl@enthought.com>
2014-08-07 16:00:51 -0700

    Working animation

--- js/vector.js
+++ js/vector.js
@@ -3,6 +3,12 @@ var mapboxTiles = L.tileLayer('https://{s}.tiles.mapbox.com/v3/tabs-enthought.j3
     attribution: '<a href="http://www.mapbox.com/about/maps/" target="_blank">Terms &amp; Feedback</a>'
 });
 
+var delay = 90;			// speed of animation (larger is slower)
+var vectorGroup = L.layerGroup([]); // global layer to update vector multiPolylines
+var isRunning = true;
+var points = [];
+var nSteps = 90;		// number of time steps to use
+
 var map = L.map('map')
     .addLayer(mapboxTiles)
     .setView([27, -94], 7);
@@ -23,10 +29,10 @@ function addRegionOutline() {
 }
 
 // parse the velocity vectors and return lines in lat/lon space
-function vectors(points, velocityVectors) {
+function getVectors(points, velocityVectors) {
     var vectors = [];
     var scale = 0.5;	// vector scaling (m/s -> degrees)
-    for (var i=0; i<points.length; i++) {
+    for (var i=0; i<nPoints; i++) {
 	var dlat = velocityVectors.v[i] * scale;
 	var dlon = velocityVectors.u[i] * scale;
 	var endpoint = [points[i][0] + dlat, points[i][1] + dlon];
@@ -42,18 +48,34 @@ function addVectorLayer(points) {
 	weight: 1,
     }
     $.getJSON('json_data/step0.json', function(json) {
-	L.multiPolyline(vectors(points, json), vectorStyle)
+	vectorGroup.addLayer(L.multiPolyline(getVectors(points, json), vectorStyle))
 	    .addTo(map);
+	showTimeStep(1);
     })
 }
 
+// update vector data at each time step
+function showTimeStep(j) {
+    var layer = vectorGroup.getLayers()[0];
+	$.getJSON('json_data/step' + j + '.json', function(json) {
+	    layer.setLatLngs(getVectors(points, json));
+	});
+    if (isRunning) {
+	nextj = (j + 1) % nSteps;
+	setTimeout(function(i) {
+	    return function() {
+		showTimeStep(i);
+	    }
+	}(nextj), delay);
+    }
+}
 
 //addRegionOutline();
 
 // put the initial velocity vectors on the map
 $.getJSON('json_data/grd_locations.json', function(json) {
-    var points = [];
-    for (var i=0; i<json['lat'].length; i++) {
+    nPoints = json['lat'].length;
+    for (var i=0; i<nPoints; i++) {
 	points.push([json.lat[i], json.lon[i]]);
     }
     addVectorLayer(points);
