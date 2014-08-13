e7b9010988e996538122a5fd08c8038e647477c4
John Tyree <johntyree@gmail.com>
2014-08-12 22:07:15 -0500

    Clean up whitespace

--- frontend/js/vector.js
+++ frontend/js/vector.js
@@ -1,42 +1,49 @@
 L.mapbox.accessToken = 'pk.eyJ1IjoidGFicy1lbnRob3VnaHQiLCJhIjoidnFxYklhcyJ9.IIjR0C-iZtw_Fr0sKUyQXQ';
 var mapboxTiles = L.tileLayer('https://{s}.tiles.mapbox.com/v3/tabs-enthought.j3nibphe/{z}/{x}/{y}.png', {
-    attribution: '<a href="http://www.mapbox.com/about/maps/" target="_blank">Terms &amp; Feedback</a>'
+    attribution: '<a href="http://www.mapbox.com/about/maps/" target="_blank">Terms &amp; Feedback</a>',
+    maxZoom: 11,
+    minZoom: 7
 });
 
-var delay = 90;			// speed of animation (larger is slower)
-var vectorGroup = L.layerGroup([]); // global layer to update vector multiPolylines
+// speed of animation (larger is slower)
+var delay = 90;
+// global layer to update vector multiPolylines
+var vectorGroup = L.layerGroup([]);
 var isRunning = true;
 var points = [];
-var nSteps = 90;		// number of time steps to use
+// number of time steps to use
+var nSteps = 90;
 
-var map = L.map('map')
-    .addLayer(mapboxTiles)
-    .setView([27, -94], 7);
+var map = L.map('map',
+    {center: [27, -94],
+     zoom: 7,
+     layers: [mapboxTiles]
+});
 
 // hard-coded region of interest outline
 function addRegionOutline() {
     var featureLayer = L.mapbox.featureLayer()
-	.loadURL('json_data/domain.json')
-	.on('ready', function(layer) {
-	    this.eachLayer(function(poly) {
-		poly.setStyle({
-		    color: "red",
-		    fill: false
-		});
-	    })
-	})
-	.addTo(map);
+        .loadURL('json_data/domain.json')
+        .on('ready', function(layer) {
+            this.eachLayer(function(poly) {
+                poly.setStyle({
+                    color: 'red',
+                    fill: false
+                });
+            });
+        })
+        .addTo(map);
 }
 
 // parse the velocity vectors and return lines in lat/lon space
 function getVectors(points, velocityVectors) {
     var vectors = [];
-    var scale = 0.5;	// vector scaling (m/s -> degrees)
-    for (var i=0; i<nPoints; i++) {
-	var dlat = velocityVectors.v[i] * scale;
-	var dlon = velocityVectors.u[i] * scale;
-	var endpoint = [points[i][0] + dlat, points[i][1] + dlon];
-	vectors.push([points[i], endpoint]);
+    var scale = 0.5;    // vector scaling (m/s -> degrees)
+    for (var i = 0; i < nPoints; i++) {
+        var dlat = velocityVectors.v[i] * scale;
+        var dlon = velocityVectors.u[i] * scale;
+        var endpoint = [points[i][0] + dlat, points[i][1] + dlon];
+        vectors.push([points[i], endpoint]);
     }
     return vectors;
 }
@@ -44,14 +51,14 @@ function getVectors(points, velocityVectors) {
 // add a vector layer to the map at the initial grid points
 function addVectorLayer(points) {
     var vectorStyle = {
-	color: 'black',
-	weight: 1,
-    }
+        color: 'black',
+        weight: 1
+    };
     $.getJSON('json_data/step0.json', function(json) {
-	vectorGroup.addLayer(L.multiPolyline(getVectors(points, json), vectorStyle))
-	    .addTo(map);
-	showTimeStep(1);
-    })
+       vectorGroup.addLayer(L.multiPolyline(getVectors(points, json), vectorStyle))
+           .addTo(map);
+       showTimeStep(1);
+    });
 }
 
 // update vector data at each time step
@@ -75,8 +82,8 @@ function showTimeStep(j) {
 // put the initial velocity vectors on the map
 $.getJSON('json_data/grd_locations.json', function(json) {
     nPoints = json['lat'].length;
-    for (var i=0; i<nPoints; i++) {
-	points.push([json.lat[i], json.lon[i]]);
+    for (var i = 0; i < nPoints; i++) {
+        points.push([json.lat[i], json.lon[i]]);
     }
     addVectorLayer(points);
 });
