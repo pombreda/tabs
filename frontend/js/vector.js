465869f8dadcfb1ade2879337322b20119daef7b
John Tyree <johntyree@gmail.com>
2014-08-15 18:23:08 -0500

    Redraw vectors on 'viewreset' (fixes #19)

--- frontend/js/vector.js
+++ frontend/js/vector.js
@@ -71,6 +71,10 @@ var TABSControl = L.Control.extend({
             }
         };
 
+        this._map.on('viewreset', function() {
+            showTimeStep(self.frame);
+        });
+
         this._redraw();
         return this.container;
     },
