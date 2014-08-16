7e2a29cd73a7f317aecd86ab6d30fe94b1c2bad3
Kelsey Jordahl <kjordahl@enthought.com>
2014-08-15 17:18:35 -0700

    ENH: serve JSON data from /data/prefetched URLs

--- app.py
+++ app.py
@@ -8,5 +8,12 @@ def index():
     return redirect(url_for('static', filename='vector.html'))
 
 
+@app.route('/data/prefetched/<time_step>')
+def prefetched(time_step):
+    """ Return static JSON data for a time step """
+    filename = 'json_data/step{}.json'.format(time_step)
+    return redirect(url_for('static', filename=filename))
+
+
 if __name__ == '__main__':
     app.run(debug=True)
