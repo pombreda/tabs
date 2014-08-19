70b18addd7fec8a0a9318163ad8a926bb669c9ca
John Tyree <johntyree@gmail.com>
2014-08-19 03:07:14 -0500

    Add not to remove FPS logging when this is all over

--- frontend/js/MapView.js
+++ frontend/js/MapView.js
@@ -153,10 +153,13 @@ MapView = (function($, L, Models) {
         var self = this;
         if (this.isRunning) {
             var t = Date.now();
+
+            // XXX: Remove eventually
             if (showFPS && ((this.currentFrame % showFPS) === 0)) {
                 console.log(showFPS / ((Date.now() - self.t) / 1000));
                 self.t = t;
             }
+
             this.showTimeStep(this.currentFrame);
             this.currentFrame = (this.currentFrame + 1) % this.nFrames;
             var waitTime = Math.max(0, this.delay - (Date.now() - t));
