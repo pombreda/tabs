f174e0072d15ecf35b23bed4aa05fc24bae5c0ec
John Tyree <johntyree@gmail.com>
2014-08-19 21:01:16 -0500

    Tidy up test file

--- scripts/test_vector_frame.py
+++ scripts/test_vector_frame.py
@@ -5,21 +5,36 @@ import os
 import subprocess
 
 import numpy as np
-np.random.seed(0xDEADBEEF)
 
 import vector_frame
 
 
+np.random.seed(0xDEADBEEF)
+vector_frame.NFRAMES = 1
+
+TEST_FILES = [os.path.join('json_data', f)
+              for f in ('step0.json', 'grd_locations.json')]
+
+
+def remove_old():
+    for f in TEST_FILES:
+        os.unlink(f)
+
+
+def tearDown():
+    remove_old()
+
+
 def diff_file(f):
-    f1 = os.path.join('json_data', 'ref_' + f)
-    f2 = os.path.join('json_data', f)
+    d, fname = os.path.split(f)
+    f_ref = os.path.join(d, 'ref_' + fname)
     try:
-        subprocess.check_call(['diff', f1, f2])
+        subprocess.check_call(['diff', f, f_ref])
     except:
-        raise AssertionError("Files differ: {!r} {!r}".format(f1, f2))
+        raise AssertionError("Files differ: {!r} {!r}".format(f, f_ref))
 
 
 def test_files():
     vector_frame.main()
-    for f in ['step0.json', 'grd_locations.json']:
+    for f in TEST_FILES:
         yield diff_file, f
