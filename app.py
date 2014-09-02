e579043b8f3e6edd68899831c2b5f85485932014
Kelsey Jordahl <kjordahl@enthought.com>
2014-09-02 17:44:40 -0500

    ENH: Serve on port 8888 and allow remote connections (but no debug)

--- app.py
+++ app.py
@@ -158,4 +158,4 @@ def static_particle_tracks():
     return redirect(url_for('static', filename=filename))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8888)
